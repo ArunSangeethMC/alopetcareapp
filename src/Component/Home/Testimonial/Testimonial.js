989edfec83c566dff1594fc20cbb058023923cc1
Jayabalaji <111067200+jayabalaji2002@users.noreply.github.com>
2024-08-03 16:17:38 +0530

    boom!

--- src/Component/Home/Testimonial/Testimonial.js
+++ src/Component/Home/Testimonial/Testimonial.js
@@ -5,7 +5,7 @@ function Testimonial() {
   return (
     <div className={testimonial.testimonial_section}>
         <div className={testimonial.testimonial_container}>
-            
+            <h1>Hello Jayabalaji from Testimonial</h1>
         </div>
     </div>
   )
